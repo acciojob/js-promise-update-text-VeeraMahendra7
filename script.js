e3924a70e8288322e745139b634bedc5151e581a
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-11-09 17:04:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let output = document.getElementById('output');
+
+function getData(){
+	return new Promise((res,rej) => {
+		setTimeout(()=>{
+			res('Hello, world!');
+		}, 1000);
+	})
+}
+
+getData().then((data) => {
+	output.textContent = data;
+})
